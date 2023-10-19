47fc09075286c0f8c63523c1a341b3ad6b8ad4fb
sejapoe <bekaldiev2015@gmail.com>
2023-10-19 14:34:58 +0300

    chore(git): 🔧 Inital commit

---
+++ back/src/main/kotlin/org/sejapoe/videomanager/VideoManagerApplication.kt
@@ -0,0 +1,11 @@
+package org.sejapoe.videomanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VideoManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<VideoManagerApplication>(*args)
+}
---
+++ back/src/test/kotlin/org/sejapoe/videomanager/VideoManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.sejapoe.videomanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VideoManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
